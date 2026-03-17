b12f495eeaadf5d79453fd3f525ee30b19ca45a1
Aman Yadav <amanyadav2303@gmail.com>
2026-03-18 03:26:45 +0530

    /createStudent api

--- server/src/middlewares/auth.js
+++ server/src/middlewares/auth.js
@@ -1,4 +1,4 @@
-const JWT_PRIVATE_KEY="@DEVTINDER$730";
+const JWT_PRIVATE_KEY="@AbyMTech$730";
 const User = require("../models/user");
 const jwt = require("jsonwebtoken");
 
@@ -6,24 +6,24 @@ const userAuth= async (req,res,next)=>{
     try {
 
         
-        //read the cookie from the request
+       
         const {token}=req.cookies;
         if(!token){
             return res.status(401).send("User not authorised. Please login.");
         }
 
-        //verify the token
+       
         const payload= await jwt.verify(token , JWT_PRIVATE_KEY);
         const {_id} = payload;
 
-        // find the user in the db 
+        
         const userData= await User.findById(_id);
         if(!userData){
             throw new Error("user does not exist.");
         }
 
-        req.user= userData; //attaching the user object(user data) with the request itself so that next request handler or middleware can read it. 
-        next(); //call the next middleware or request handler
+        req.user= userData; 
+        next();
 
 
 
--- server/src/routes/student.js
+++ server/src/routes/student.js
@@ -1,9 +1,41 @@
 const express = require("express");
 const { userAuth } = require("../middlewares/auth");
-
+const Student = require("../models/student");
 const studentRouter = express.Router();
+const validator = require("validator");
+
 
 studentRouter.post("/createStudent",userAuth,
-    (req,res)=>{
-        
+    async (req, res) => {
+    try {
+        const { firstName, lastName, email, rollNum, course } = req.body;
+
+        if(!firstName || !lastName){
+            throw new Error("First and Last Name is required");
+        }
+        else if(!validator.isEmail(email)){
+            throw new Error("Invalid email");
+        }
+        else if(!course){
+            throw new Error("course is required");
+        }
+        else if(!(/^[1-9]\d{2}\/[A-Z]{3}\/\d{3}$/.test(rollNum))){
+            throw new Error("Invalid roll number format. Expected format: 123/ABC/456");
+        }
+
+        const student = new Student({
+            firstName,
+            lastName,
+            email,
+            rollNum,
+            course
+        });
+
+        await student.save();
+
+        res.status(201).send({ message: "Student created successfully", data: student });
+
+    } catch (err) {
+        res.status(400).send("Error creating student: " + err.message);
+    }
 });
